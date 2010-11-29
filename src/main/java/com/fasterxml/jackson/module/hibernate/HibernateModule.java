aa61f0260d55eefabdce5e71201f4a063d5be55d
Tatu Saloranta <tsaloranta@gmail.com>
2010-11-28 18:37:01 -0800

    Adding minimal skeletal sources to get project started

---
+++ src/main/java/com/fasterxml/jackson/module/hibernate/HibernateModule.java
@@ -0,0 +1,25 @@
+package com.fasterxml.jackson.module.hibernate;
+
+import org.codehaus.jackson.Version;
+import org.codehaus.jackson.map.*;
+
+public class HibernateModule extends Module
+{
+    private final String NAME = "HibernateModule";
+    
+    // Should externalize this somehow
+    private final static Version VERSION = new Version(0, 1, 0, null); // 0.1.0
+
+    public HibernateModule() { }
+
+    @Override public String getModuleName() { return NAME; }
+    @Override public Version version() { return VERSION; }
+    
+    @Override
+    public void setupModule(SetupContext context)
+    {
+        // !!! TODO:
+//        context.addSerializers(new HibernateSerializers());
+    }
+
+}
