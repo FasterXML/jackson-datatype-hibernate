5df772280cd418293830b8582ff3cb62700a6211
sjbwylbs <sjbwylbs@gmail.com>
2012-02-29 21:13:05 +0800

    for hibernate 4.X
    update for hibernate 4.X version

--- src/main/java/com/fasterxml/jackson/datatype/hibernate/HibernateSerializers.java
+++ src/main/java/com/fasterxml/jackson/datatype/hibernate/HibernateSerializers.java
@@ -2,8 +2,8 @@ package com.fasterxml.jackson.datatype.hibernate;
 
 import java.util.*;
 
-import org.hibernate.collection.PersistentCollection;
-import org.hibernate.collection.PersistentMap;
+import org.hibernate.collection.internal.PersistentMap;
+import org.hibernate.collection.spi.PersistentCollection;
 import org.hibernate.proxy.HibernateProxy;
 
 import com.fasterxml.jackson.databind.*;
