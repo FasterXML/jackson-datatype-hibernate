89be11035d701c92969613bebb5ee3dd15d006f2
Michael May <goldenalfer@gmail.com>
2012-08-11 12:06:06 +0600

    Spaces fix for normal diff

--- src/main/java/com/fasterxml/jackson/datatype/hibernate3/Hibernate3Module.java
+++ src/main/java/com/fasterxml/jackson/datatype/hibernate3/Hibernate3Module.java
@@ -27,7 +27,15 @@ public class Hibernate3Module extends Module
          * 
          * @since 0.7.0
          */
-        USE_TRANSIENT_ANNOTATION(true)
+        USE_TRANSIENT_ANNOTATION(true),
+
+		/**
+		 * If FORCE_LAZY_LOADING is false lazy-loaded object should be serialized as map IdentifierName=>IdentifierValue
+		 * istead of null (true); or serialized as nulls (false)
+		 * <p>
+		 * Defaul value is false
+		 */
+		SERIALIZE_IDENTIFIER_FOR_LAZY_NOT_LOADED_OBJECTS(false)
         ;
 
         final boolean _defaultState;
