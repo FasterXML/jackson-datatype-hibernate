b1b2ed91e05573614120c74bfbefa5425ead01c5
Michael May <goldenalfer@gmail.com>
2012-08-13 11:53:52 +0600

    Revert "Feature. SERIALIZE_IDENTIFIER_FOR_LAZY_NOT_LOADED_OBJECTS"
    This reverts commit ea09ff6ae9ed7989bb03f03ed2ad93f7ad024657.

--- src/main/java/com/fasterxml/jackson/datatype/hibernate4/Hibernate4Module.java
+++ src/main/java/com/fasterxml/jackson/datatype/hibernate4/Hibernate4Module.java
@@ -26,15 +26,7 @@ public class Hibernate4Module extends Module
          * Default value is true.
          */
         USE_TRANSIENT_ANNOTATION(true)
-
-		/**
-		 * If FORCE_LAZY_LOADING is false lazy-loaded object should be serialized as map IdentifierName=>IdentifierValue
-		 * istead of null (true); or serialized as nulls (false)
-		 * <p>
-		 * Defaul value is false
-		 */
-		SERIALIZE_IDENTIFIER_FOR_LAZY_NOT_LOADED_OBJECTS(false)
-		;
+        ;
 
         final boolean _defaultState;
         final int _mask;
