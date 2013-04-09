2cea0d6fd8906858b8bc529973a43b5ca2e4a9d3
Tatu Saloranta <tsaloranta@gmail.com>
2013-04-09 14:15:02 -0700

    Converting hibernate4 module to use the new version introspection mechanism

--- hibernate4/src/test/java/com/fasterxml/jackson/datatype/hibernate4/TestVersions.java
+++ hibernate4/src/test/java/com/fasterxml/jackson/datatype/hibernate4/TestVersions.java
@@ -6,14 +6,6 @@ import com.fasterxml.jackson.datatype.hibernate4.Hibernate4Module;
 
 public class TestVersions extends BaseTest
 {
-    // Not a good way to do this, but has to do, for now...
-    private final static int MAJOR_VERSION = 2;
-    private final static int MINOR_VERSION = 2;
-
-    // could inject using Maven filters as well...
-    private final static String GROUP_ID = "com.fasterxml.jackson.datatype";
-    private final static String ARTIFACT_ID = "jackson-datatype-hibernate4";
-    
     public void testMapperVersions()
     {
         assertVersion(new Hibernate4Module());
@@ -24,17 +16,13 @@ public class TestVersions extends BaseTest
     /* Helper methods
     /**********************************************************
      */
-    
     private void assertVersion(Versioned vers)
     {
-        final Version v = vers.version();
+        Version v = vers.version();
         assertFalse("Should find version information (got "+v+")", v.isUknownVersion());
-        assertEquals(MAJOR_VERSION, v.getMajorVersion());
-        assertEquals(MINOR_VERSION, v.getMinorVersion());
-        // Check patch level initially, comment out for maint versions
-//        assertEquals(0, v.getPatchLevel());
-        assertEquals(GROUP_ID, v.getGroupId());
-        assertEquals(ARTIFACT_ID, v.getArtifactId());
+        Version exp = PackageVersion.VERSION;
+        assertEquals(exp.toFullString(), v.toFullString());
+        assertEquals(exp, v);
     }
 }
 
