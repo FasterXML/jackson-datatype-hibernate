81e05328847e909364d666ff074c0eaa3ce3965c
Tatu Saloranta <tatu.saloranta@iki.fi>
2015-12-27 22:06:47 -0800

    move newly added failing test (for #70) under failing so as not to break CI

--- hibernate4/src/test/java/com/fasterxml/jackson/datatype/hibernate4/failing/InfiniteRecursion70Test.java
+++ hibernate4/src/test/java/com/fasterxml/jackson/datatype/hibernate4/failing/InfiniteRecursion70Test.java
@@ -1,25 +1,24 @@
-package com.fasterxml.jackson.datatype.hibernate4;
+package com.fasterxml.jackson.datatype.hibernate4.failing;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 import org.hibernate.Hibernate;
-import org.junit.Test;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.hibernate4.BaseTest;
 import com.fasterxml.jackson.datatype.hibernate4.data.Contrato;
 
-public class InfiniteRecursionTest extends BaseTest {
+public class InfiniteRecursion70Test extends BaseTest {
 
-	// [Issue#70]
-    @Test
-	public void testInfinite() throws Exception {
-		
-    	 EntityManagerFactory emf = Persistence.createEntityManagerFactory("persistenceUnit");
-    	 String expected = "{\"id\":1,\"numeroContrato\":\"100001-9\",\"parcelas\":[{\"id\":1,\"numeroParcela\":1}],\"liquidacoes\":[{\"id\":1,\"valorTotal\":10000,\"parcela\":{\"id\":1,\"numeroParcela\":1}}]}";
+	// [datatype-hibernate#70]
+    public void testInfinite() throws Exception
+    {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("persistenceUnit");
+        String expected = "{\"id\":1,\"numeroContrato\":\"100001-9\",\"parcelas\":[{\"id\":1,\"numeroParcela\":1}],\"liquidacoes\":[{\"id\":1,\"valorTotal\":10000,\"parcela\":{\"id\":1,\"numeroParcela\":1}}]}";
     	 
-         try {
+        try {
              EntityManager em = emf.createEntityManager();
              ObjectMapper mapper = mapperWithModule(true);
              
@@ -39,7 +38,5 @@ public class InfiniteRecursionTest extends BaseTest {
          } finally {
              emf.close();
          }
-    	
 	}
-	
 }
