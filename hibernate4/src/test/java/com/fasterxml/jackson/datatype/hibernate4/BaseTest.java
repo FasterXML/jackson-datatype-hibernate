fd6039b5f267de549862bc826dacecba419fe30e
Tatu Saloranta <tatu.saloranta@iki.fi>
2025-02-25 17:06:14 -0800

    Fix h2 compatibility issue for Hibernate4 module

--- hibernate4/src/test/java/com/fasterxml/jackson/datatype/hibernate4/BaseTest.java
+++ hibernate4/src/test/java/com/fasterxml/jackson/datatype/hibernate4/BaseTest.java
@@ -3,7 +3,6 @@ package com.fasterxml.jackson.datatype.hibernate4;
 import java.util.Arrays;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.hibernate4.Hibernate4Module;
 
 public abstract class BaseTest extends junit.framework.TestCase
 {
